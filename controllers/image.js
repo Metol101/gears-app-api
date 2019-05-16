765a8745e3f0fc2eb357fc86c3170308296fc991
Moustafa Hassan <metolmain@gmail.com>
2019-05-16 17:21:43 +0200

    Tweaking api key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-   apiKey: '42961f00b5e048d684b2e15f0044495d'
+   apiKey: process.env.API_CLARIFAI_KEY
   });
 
 const handleApiCall = (req, res) => {
